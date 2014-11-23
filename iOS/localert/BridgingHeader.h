2e7f574500b6f3b8d307b5e3f7054de36cad1d88
Daniel Person <danielperson@commandcenter.local>
2014-11-23 14:10:27 -0500

    first commit with initial header includes

---
+++ iOS/localert/BridgingHeader.h
@@ -0,0 +1,18 @@
+//
+//  BridgingHeader.h
+//  localert
+//
+//  Created by Daniel Person on 11/23/14.
+//  Copyright (c) 2014 Orenda LLC. All rights reserved.
+//
+
+#ifndef localert_BridgingHeader_h
+#define localert_BridgingHeader_h
+
+#include "AFNetworking.h"
+#include "Bolts.h"
+#include "NBAsYouTypeFormatter.h"
+#include "NBPhoneNumber.h"
+#include "NBPhoneNumberUtil.h"
+
+#endif
