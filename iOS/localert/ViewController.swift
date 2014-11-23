f58603b71485661d58362f38c2442ad95daa863a
Daniel Person <danielperson@commandcenter.local>
2014-11-23 13:48:55 -0500

    first commit

---
+++ iOS/localert/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  localert
+//
+//  Created by Daniel Person on 11/23/14.
+//  Copyright (c) 2014 Orenda LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
